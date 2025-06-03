912d73ba3338e2d152bf9745102673ba41d57e11
Abdullah-Zulfikar <B22F0275AI131@fecid.paf-iast.edu.pk>
2025-06-03 22:24:54 +0500

    feat: add mediasoup dependency and configure media codecs in config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
+
 import os from "os";
+
 
 export const config = {
     listenIp: '0.0.0.0',
@@ -19,5 +21,33 @@ export const config = {
                 'rtcp',
             ] as ('info' | 'ice' | 'dtls' | 'rtp' | 'srtp' | 'rtcp')[],
         },
+        router: {
+            mediaCodecs: [
+                {
+                    kind: "audio",
+                    mimeType: 'audio/opus',
+                    clockRate: 48000,
+                    channels:2
+                }, 
+                {
+                    kind: "video",
+                    mimeType:'video/VP8',
+                    clockRate: 90000,
+                    parameters: {
+                        'x-google-start-bitrate': 1000
+                    }
+                },
+            ] 
+        },
+        // webtrctransport Settings
+        webRtcTransport : {
+            listenIps: [
+                {
+                    ip: '0.0.0.0',
+                    announcedIp: '127.0.0.1'  //replace it with your public ip while hosting
+
+                }
+            ] 
+        },
     }
-}
+} as const;
