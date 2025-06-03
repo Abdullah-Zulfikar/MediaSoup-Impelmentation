4229c93691c559724f7d375a81a85d71868f1669
Abdullah-Zulfikar <B22F0275AI131@fecid.paf-iast.edu.pk>
2025-06-03 22:45:16 +0500

    feat: implement WebSocket connection handling and create worker functionality

--- src/lib/ws.ts
+++ src/lib/ws.ts
@@ -1,7 +1,12 @@
 import { WebSocket, Server } from "ws";
+import { createWorker } from "./worker";
 
 const WebscoketConnect = async (websock: Server) => {
-
+    try{
+        const mediasoupRouter = await createWorker();
+    }catch(error) {
+        throw error;
+    }
     websock.on('connection', (ws: WebSocket) => {
         ws.on('message', (message: string) => {
             console.log("Message =>", message)
